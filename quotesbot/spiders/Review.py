f29dff3264a28c7878d22164ed2f0e66fb515e2a
giangvu96 <vutruonggiang1996@gmail.com>
2018-10-03 20:30:11 +0700

    Review product

--- quotesbot/spiders/Review.py
+++ quotesbot/spiders/Review.py
@@ -1,11 +1,35 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+#Book
 class Review(scrapy.Spider):
     name = 'review'
-    start_urls = ['https://www.amazon.com/Office-Style-Powered-Desktop-Calculator/product-reviews/B00J1VB2WY/ref=cm_cr_getr_d_paging_btm_1?ie=UTF8&reviewerType=all_reviews&pageNumber=1']
+    # start_urls = ['https://www.amazon.com/Office-Style-Powered-Desktop-Calculator/product-reviews/B00J1VB2WY/ref=cm_cr_getr_d_paging_btm_1?ie=UTF8&reviewerType=all_reviews&pageNumber=1',
+    #               'https://www.amazon.com/Differential-Equations-Scientists-Engineers-Mathematics-ebook/product-reviews/B008TVLUTW/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews&pageNumber=1']
+
+    start_urls = ['https://www.amazon.com/Know-Why-Caged-Bird-Sings/dp/0345514408/ref=lp_45_1_1?s=books&ie=UTF8&qid=1538571867&sr=1-1']
 
     def parse(self, response):
+        see_all_review = response.xpath('//a[@id="dp-summary-see-all-reviews"]')
+        if see_all_review is not None:
+            url = 'https://www.amazon.com' + see_all_review.xpath('./@href').extract_first()
+            yield scrapy.Request(url, callback=self.parseWithSeeAllReview)
+        else:
+            for review in response.xpath('//div[@id="cm_cr-review_list"]/div[@class="a-section review"]'):
+                yield {
+                    'rating': review.xpath('./div/div/a/i/span[@class="a-icon-alt"]/text()').extract_first().split(' ')[
+                        0],
+                    'review_title': review.xpath(
+                        './div/div/a[@class="a-size-base a-link-normal review-title a-color-base a-text-bold"]/text()').extract_first(),
+                    # 'color': review.xpath('//a[@class="a-size-mini a-link-normal a-color-secondary"]/text()').extract_first(),
+                    'review_body': review.xpath(
+                        './div/div/span[@class="a-size-base review-text"]/text()').extract_first(),
+                }
+            next = response.xpath('//a[contains(text(),"Next")]/@href').extract_first()
+            if next is not None:
+                next_page = 'https://www.amazon.com' + next
+                yield scrapy.Request(next_page, callback=self.parse)
+
+    def parseWithSeeAllReview(self, response):
         for review in response.xpath('//div[@id="cm_cr-review_list"]/div[@class="a-section review"]'):
             yield {
                 'rating': review.xpath('./div/div/a/i/span[@class="a-icon-alt"]/text()').extract_first().split(' ')[0],
@@ -16,32 +40,4 @@ class Review(scrapy.Spider):
         next = response.xpath('//a[contains(text(),"Next")]/@href').extract_first()
         if next is not None:
             next_page = 'https://www.amazon.com' + next
-            yield scrapy.Request(next_page, callback=self.parse)
-
-    # def parse2(self, response):
-    #     for quote in response.xpath('//div[@class="review-container"]'):
-    #         yield {
-    #             'noQuotes': quote.xpath('.//span[@class="noQuotes"]/text()').extract_first(),
-    #             'partial_entry': quote.xpath('.//p[@class="partial_entry"]/text()').extract_first(),
-    #         }
-        # next_page_xxx = response.xpath(
-        #     '//div[@class="listContainer hide-more-mobile"]/div[@class="mobile-more"]/div[@class="prw_rup prw_common_responsive_pagination"]/div/a[2]/@href').extract_first()
-        # if next_page_xxx is not None:
-        #     next_page = 'https://www.tripadvisor.com' + next_page_xxx
-        #     yield scrapy.Request(next_page_xxx, callback=self.parse)
-        # else:
-        #     self.driver.close()
-
-    # def parse(self, response):
-    #     yield {
-    #         'more': response.xpath('//div[@class="review-container"]/div/div/div/div/div[@class="prw_rup prw_reviews_text_summary_hsx"]/div/p/span[contains(text(),"More")]/@class').extract_first(),
-    #     }
-        # for quote in response.xpath('//div[@class="review-container"]'):
-        #     yield {
-        #         'noQuotes': quote.xpath('.//span[@class="noQuotes"]/text()').extract_first(),
-        #         'partial_entry': quote.xpath('.//p[@class="partial_entry"]/text()').extract_first(),
-        #     }
-        # next_page = response.xpath('//div[@class="listContainer hide-more-mobile"]/div[@class="mobile-more"]/div[@class="prw_rup prw_common_responsive_pagination"]/div/a[2]/@href').extract_first()
-        # if next_page is not None:
-        #     next_page = 'https://www.tripadvisor.com' + next_page
-        #     yield scrapy.Request(next_page, callback=self.parse)
+            yield scrapy.Request(next_page, callback=self.parseWithSeeAllReview)

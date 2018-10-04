3397527bc83f614cba7187a110cc9ddbed746188
giangvu96 <vutruonggiang1996@gmail.com>
2018-10-04 22:41:17 +0700

    Review product Fix

--- quotesbot/spiders/Review.py
+++ quotesbot/spiders/Review.py
@@ -1,17 +1,38 @@
 # -*- coding: utf-8 -*-
 import scrapy
 #Book
+
+
 class Review(scrapy.Spider):
     name = 'review'
-    # start_urls = ['https://www.amazon.com/Office-Style-Powered-Desktop-Calculator/product-reviews/B00J1VB2WY/ref=cm_cr_getr_d_paging_btm_1?ie=UTF8&reviewerType=all_reviews&pageNumber=1',
-    #               'https://www.amazon.com/Differential-Equations-Scientists-Engineers-Mathematics-ebook/product-reviews/B008TVLUTW/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews&pageNumber=1']
-
-    start_urls = ['https://www.amazon.com/Know-Why-Caged-Bird-Sings/dp/0345514408/ref=lp_45_1_1?s=books&ie=UTF8&qid=1538571867&sr=1-1']
+    start_urls = [
+                  'https://www.amazon.com/Differential-Equations-Scientists-Engineers-Mathematics-ebook/product-reviews/B008TVLUTW/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews&pageNumber=1']
+    # start_urls = ['https://www.amazon.com/s/ref=lp_7072561011_nr_n_0?fst=as%3Aoff&rh=n%3A2335752011%2Cn%3A%212335753011%2Cn%3A7072561011%2Cn%3A2407748011&bbn=7072561011&ie=UTF8&qid=1538584000&rnid=7072561011']
+    # def parse(self, response):
+    #     # searchResults = response.xpath('//div[@id="search-results"]')
+    #     mainResults = response.xpath('//div[@id="mainResults"]')
+    #     if mainResults is not None:
+    #         for list in mainResults.xpath('./ul/li'):
+    #             link = list.xpath('./div/div[@class="a-row a-spacing-mini"]/div[@class="a-row a-spacing-none sx-line-clamp-4"]/a/@href').extract_first()
+    #             if link is not None:
+    #                 yield {
+    #                     'link': link
+    #                 }
+    #
+    #         next = response.xpath('//a[@id="pagnNextLink"]/@href').extract_first()
+    #         if next is not None:
+    #             next_page = 'https://www.amazon.com' + next
+    #             yield scrapy.Request(next_page, callback=self.parse)
+    #
+    #     #     next = response.xpath('//a[contains(text(),"Next")]/@href').extract_first()
+    #     #     if next is not None:
+    #     #         next_page = 'https://www.amazon.com' + next
+    #     #         yield scrapy.Request(next_page, callback=self.parse)
 
     def parse(self, response):
-        see_all_review = response.xpath('//a[@id="dp-summary-see-all-reviews"]')
+        see_all_review = response.xpath('//a[@id="dp-summary-see-all-reviews"]/@href').extract_first()
         if see_all_review is not None:
-            url = 'https://www.amazon.com' + see_all_review.xpath('./@href').extract_first()
+            url = 'https://www.amazon.com' + see_all_review
             yield scrapy.Request(url, callback=self.parseWithSeeAllReview)
         else:
             for review in response.xpath('//div[@id="cm_cr-review_list"]/div[@class="a-section review"]'):
